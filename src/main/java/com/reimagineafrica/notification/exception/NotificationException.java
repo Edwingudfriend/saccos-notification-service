30c8a7c6b6b26e4c8187220eb27088e0d5eed3df
@EdwinPNchia <edwingudfriend@gmail.com>
2026-05-28 12:37:30 +0300

    add: exception

---
+++ src/main/java/com/reimagineafrica/notification/exception/NotificationException.java
@@ -0,0 +1,5 @@
+package com.reimagineafrica.notification.exception;
+
+public class NotificationException extends RuntimeException {
+    public NotificationException(String message) { super(message); }
+}
