965e8645057c7299182b6833fe6efa62a62a1418
@EdwinPNchia <edwingudfriend@gmail.com>
2026-05-28 12:37:05 +0300

    add: main

--- src/main/java/com/reimagineafrica/notification/NotificationServiceApplication.java
+++ src/main/java/com/reimagineafrica/notification/NotificationServiceApplication.java
@@ -2,10 +2,10 @@ package com.reimagineafrica.notification;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
-@EnableScheduling
+@EnableAsync
 public class NotificationServiceApplication {
     public static void main(String[] args) {
         SpringApplication.run(NotificationServiceApplication.class, args);
