b8dca937620a2978a90590b955649ae3a950990b
@EdwinPNchia <edwingudfriend@gmail.com>
2026-05-28 20:53:30 +0300

    fix: move @EnableRabbit to public main class

--- src/main/java/com/reimagineafrica/notification/NotificationServiceApplication.java
+++ src/main/java/com/reimagineafrica/notification/NotificationServiceApplication.java
@@ -1,11 +1,15 @@
 package com.reimagineafrica.notification;
 
+import org.springframework.amqp.rabbit.annotation.EnableRabbit;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableAsync
+@EnableScheduling
+@EnableRabbit
 public class NotificationServiceApplication {
     public static void main(String[] args) {
         SpringApplication.run(NotificationServiceApplication.class, args);
