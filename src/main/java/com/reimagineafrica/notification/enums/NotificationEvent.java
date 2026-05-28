fe29bc113880a532d7e38768eb5ecc8ad770bdaa
@EdwinPNchia <edwingudfriend@gmail.com>
2026-05-28 12:31:13 +0300

    add: NotificationEvent

---
+++ src/main/java/com/reimagineafrica/notification/enums/NotificationEvent.java
@@ -0,0 +1,15 @@
+package com.reimagineafrica.notification.enums;
+
+public enum NotificationEvent {
+    MEMBER_CREATED,
+    LOAN_APPROVED,
+    LOAN_REJECTED,
+    LOAN_DISBURSED,
+    CONTRIBUTION_MISSED,
+    GUARANTOR_CONSENT_REQUESTED,
+    DIVIDEND_DECLARED,
+    PASSWORD_RESET,
+    LOGIN_MFA,
+    ACCOUNT_LOCKED,
+    CUSTOM
+}
